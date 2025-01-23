d6a7d317b418f9a1b39385b82a2ca5912ba91bde
Rupert Señga <pertsenga@gmail.com>
2025-01-23 12:38:53 +0800

    test: Add specs for Passweid::Checker class

---
+++ spec/passweird/checker_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe Passweird::Checker do
+  describe ".blacklisted?" do
+    describe "when the password is blacklisted" do
+      before do
+        Passweird::BlacklistedTerm.create(term: "word")
+      end
+
+      it "returns true" do
+        expect(described_class.blacklisted?("word")).to be true
+      end
+
+      describe "where a substring of the password is blacklisted" do
+        it "returns true" do
+          expect(described_class.blacklisted?("password")).to be true
+        end
+      end
+
+      describe "where a leet speak version of the password substring is blacklisted" do
+        it "returns true" do
+          expect(described_class.blacklisted?("p455w0rd")).to be true
+        end
+      end
+    end
+
+    describe "when the password is NOT blacklisted" do
+      before do
+        Passweird::BlacklistedTerm.create(term: "anotherword")
+      end
+
+      it "returns false" do
+        expect(described_class.blacklisted?("password")).to be false
+      end
+    end
+  end
+end
