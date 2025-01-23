96964ece16965cf374ae6c313f383c93dabb9530
Rupert Señga <pertsenga@gmail.com>
2025-01-23 12:12:58 +0800

    refactor: Allow class to leet and unleet the string

--- lib/passweird/leet_speak.rb
+++ lib/passweird/leet_speak.rb
@@ -2,14 +2,16 @@
 
 module Passweird
   # The LeetSpeak class is responsible for converting a given string
-  # into leet speak (1337 5p34k).
+  # into leet speak (1337 5p34k) and converting it back to normal text.
   #
   # Example usage:
   #   leet_speak = Passweird::LeetSpeak.new("example")
-  #   leet_string = leet_speak.convert
+  #   leet_string = leet_speak.leet
   #   # => "3x4mpl3"
+  #   normal_string = leet_speak.unleeted
+  #   # => "example"
   class LeetSpeak
-    attr_reader :original_string
+    attr_reader :given_string
 
     LEET_CHAR_EQUIVALENTS = {
       "T" => "7", "t" => "7",
@@ -23,27 +25,44 @@ module Passweird
       "B" => "8", "b" => "8"
     }.freeze
 
-    def self.convert(original_string)
-      new(original_string).convert
+    def self.leet(given_string)
+      new(given_string).leet
     end
 
-    def self.convert_all(array_of_strings)
+    def self.leet_all(array_of_strings)
       raise ArgumentError, "array_of_strings must be an Array" unless array_of_strings.is_a?(Array)
 
-      array_of_strings.map { |string| convert(string) }
+      array_of_strings.map { |string| leet(string) }
     end
 
-    def initialize(original_string)
-      raise ArgumentError, "original_string must be a String" unless original_string.is_a?(String)
-
-      @original_string = original_string
+    def self.unleeted(given_string)
+      new(given_string).unleeted
     end
 
-    # Converts the original_string to leet speak
+    def self.unleet_all(array_of_strings)
+      raise ArgumentError, "array_of_strings must be an Array" unless array_of_strings.is_a?(Array)
+
+      array_of_strings.map { |string| unleeted(string) }
+    end
+
+    def initialize(given_string)
+      raise ArgumentError, "given_string must be a String" unless given_string.is_a?(String)
+
+      @given_string = given_string
+    end
+
+    # Converts the given_string to leet speak
     #
     # @return [String] the converted leet speak string
-    def convert
-      original_string.gsub(/[#{LEET_CHAR_EQUIVALENTS.keys.join}]/, LEET_CHAR_EQUIVALENTS)
+    def leet
+      given_string.gsub(/[#{LEET_CHAR_EQUIVALENTS.keys.join}]/, LEET_CHAR_EQUIVALENTS)
+    end
+
+    # Converts the leet speak string back to normal text
+    #
+    # @return [String] the converted normal text string
+    def unleeted
+      given_string.gsub(/[#{LEET_CHAR_EQUIVALENTS.values.join}]/, LEET_CHAR_EQUIVALENTS.invert)
     end
   end
 end
