3292fd4ed923db22edaa5143cadb648c8ab7187b
Rupert Señga <pertsenga@gmail.com>
2025-01-23 10:37:50 +0800

    feat: Load the class into the Gem

--- lib/passweird.rb
+++ lib/passweird.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "passweird/version"
+require_relative "passweird/blacklisted_term"
+require_relative "passweird/checker"
+require_relative "passweird/substringer"
+require_relative "passweird/leet_speak"
 
 module Passweird
   class Error < StandardError; end
